d23311c7de5ddf792dc9b6b3345b5a3a6ad0a8ce
Szymon Ochnio <szymonochnio15@gmail.com>
2024-05-14 23:52:48 +0200

    Start solving ownapipro task

--- api_tasks/ownapipro.py
+++ api_tasks/ownapipro.py
@@ -1,14 +1,15 @@
 import json
 from tools.aidevs import get_token, get_task, send_answer
 
-print('C04L04 ownapi')
+print('C04L05 ownapipro')
 
 # fetch token and task
-token = get_token('ownapi')
+token = get_token('ownapipro')
 task = get_task(token)
 task_json = json.loads(task)
 print('\ntask_json: ', task_json)
 
+exit()
 # send my api address as answer
 result = send_answer(token, "https://hook.eu2.make.com/{mywebhook}")
 print('\nresult: ', result)
