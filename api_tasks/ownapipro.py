3009dd864281c844f34ab9a3972114a4ae0054a1
Szymon Ochnio <szymonochnio15@gmail.com>
2024-05-15 23:11:00 +0200

    Solve ownapipro task

--- api_tasks/ownapipro.py
+++ api_tasks/ownapipro.py
@@ -9,7 +9,6 @@ task = get_task(token)
 task_json = json.loads(task)
 print('\ntask_json: ', task_json)
 
-exit()
 # send my api address as answer
 result = send_answer(token, "https://hook.eu2.make.com/{mywebhook}")
 print('\nresult: ', result)
